b6b97cf3f3f9b700e69d0b6fa6470104efacb63a
S4ji <edel.e.devweb@gmail.com>
2025-07-01 15:26:58 +0200

    fix/formswitcher

--- components/FormSwitcher.vue
+++ components/FormSwitcher.vue
@@ -133,7 +133,7 @@ async function fetchUserOeuvres(userId) {
 // Requête collections
 async function fetchUserCollections(userId) {
     try {
-        const res = await fetch(`${API_URL}/collections/user/${userId}`)
+        const res = await fetch(`${API_URL}/collections/user/${currentUserId}`)
         if (!res.ok) throw new Error('Failed to fetch collections')
 
         const data = await res.json()
