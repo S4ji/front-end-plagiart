b2a6d9c5c04527f5330b6acf00a038498a217c64
S4ji <edel.e.devweb@gmail.com>
2025-07-02 10:03:08 +0200

    fix/css footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,19 @@
 <template>
     <footer class="my-20">
-        <p class="text-center text-sm text-slate-500">
+        <p :class="footerTextClass">
             Copyright © {{ new Date().getFullYear() }} Plagi'arism
         </p>
 
-        <p v-if="isLoggedIn" class="text-gray-700">
+        <p v-if="isLoggedIn" :class="footerTextClass">
             Bonjour, {{ userName || 'Utilisateur' }}
         </p>
     </footer>
 </template>
+
 <script setup>
 import { useAuth } from '@/composables/useAuth'
 
 const { userName, isLoggedIn } = useAuth()
+
+const footerTextClass = 'text-center text-sm text-slate-500'
 </script>
